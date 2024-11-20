f38f0a7d864d100f27939d95f9dda7a3bed86e1f
Manuel Ntanfumu <164122296+manntanfumu0@users.noreply.github.com>
2024-11-20 15:10:47 -0300

    Operadores
    Os operadores em Java são símbolos ou combinações de símbolos usados para realizar operações em variáveis e valores. 
    Eles podem ser classificados em várias categorias como:
    
    1. Operadores Aritméticos
    Usados para realizar operações matemáticas básicas.
    
    Operadores:
    
    +	Adição	
    -	Subtração	
    *	Multiplicação	
    /	Divisão	
    %	Módulo (resto)	
    
    
    2. Operadores Relacionais (Comparação)
    Usados para comparar dois valores. 
    
    Operador	es:	
    
    ==	Igualdade	
    !=	Diferente	
    >	Maior que	
    <	Menor que	
    >=	Maior ou igual	
    <= Menor ou igual
    
    3. Operadores Lógicos
    Usados para combinar expressões booleanas
    
    Operadores:
    
    AND && = Saída só será verdadeira se todas as condições de entrada forem verdadeiras
    OR || = Saída só será verdadeira se uma das condições de entrada for  verdadeira
    NOT = Saída contraria a entrada

---
+++ Operadores1.java
@@ -0,0 +1,23 @@
+public class Operadores1 {
+    public static void main(String[] args) {
+        System.out.println("Uso de variáveis na linguagem Java");
+        System.out.println("Exemplos");
+        double i = 10;
+        System.out.println(" Operadores aritméticos e Atribuições");
+        System.out.println("Exemplos");
+        System.out.println("i = " + i);
+        System.out.println("i = " + i + " + 5 | i = " + (i + 5));
+        System.out.println("i = " + i + " - 5 | i = " + (i - 5));
+        System.out.println("i = " + i + " * 5 | i = " + (i * 5));
+        System.out.println("i = " + i + " / 5 | i = " + (i / 5));
+        System.out.println("i = " + i + " % 5 | i = " + (i % 5));
+        System.out.println("i += 5        |i = " + (i += 5));
+        System.out.println("i -= 5        |i = " + (i -= 5));
+        System.out.println("i *= 5        |i = " + (i *= 5));
+        System.out.println("i /= 5        |i = " + (i /= 5));
+        i++;
+        System.out.println("i++              | i = " + i);
+        i--;
+        System.out.println("i--             | i = " + i);
+    }
+}
