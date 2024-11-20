25aead4876764faa8cbd2217ad555f1127eed9fa
Manuel Ntanfumu <164122296+manntanfumu0@users.noreply.github.com>
2024-11-20 14:54:35 -0300

    Fundamentos da programação em Java parte 2
    Nesta parte 2 irei mostrar alguns dos tipos de variáveis, é bem sabido que elas se dividem em duas partes:
    
    - Tipos  primitivos 
    - Não primitivos 
    
    Tipos Primitivos
    São os tipos básicos de dados fornecidos pela linguagem e armazenam valores diretamente na memória.
    Exemplo: Numéricos Inteiros ; Numéricos Decimais; Caractere único e Valores lógicos.
    
    Tipos Não Primitivos 
    São mais complexos e referenciam objetos na memória, ao invés de armazenarem valores diretamente.
    Exemplos: Strings; Arrays; Classes e Objetos e Coleções

---
+++ Fundamentos2.java
@@ -0,0 +1,17 @@
+package cursoJava;
+
+public class Fundamnetos2 {
+    public static void main(String[] args) {
+        String nome;
+        nome = "Manuel Filipe";
+        int idade = 22;
+        char sexo ='M';
+        double temperatura = 26.7;
+        boolean arCondicionado = false;
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Temperatura: " + temperatura);
+        System.out.println("Ar condicionado: " + arCondicionado);
+    }
+}
