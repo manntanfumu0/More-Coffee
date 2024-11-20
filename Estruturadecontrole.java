b9265b69206b1a20b271da32c861fcffb84a8e17
Manuel Ntanfumu <164122296+manntanfumu0@users.noreply.github.com>
2024-11-20 15:17:49 -0300

    Estruturas de controle ( Decisão )
    As estruturas de controle em Java são usadas para controlar o fluxo de execução do programa com base em condições ou repetições. Elas podem ser classificadas em estruturas de decisão e estruturas de repetição.
    
    1. Estruturas de Decisão (Condicional)
    Permitem que o programa tome decisões com base em condições.
    
    a) if, else if, else

---
+++ Estruturadecontrole.java
@@ -0,0 +1,42 @@
+public class Estruturasdecontrole {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Estruturas de controle");
+        System.out.println("");
+        System.out.println("Estruturas de controle condicional");
+        System.out.println("Exemplo 1 uso do 'if'");
+        char sexo = 'f';
+            if ( sexo == 'M'){System.out.println("Alistamento militar obrigatorio");
+        }
+        System.out.println("");
+            System.out.println("");
+            System.out.println("Exemplo 2 uso do 'if=else'");
+            int idade = 17;
+            if ( idade < 18 ){
+            System.out.println("Menor de idade");
+            }
+            else {
+                System.out.println("Maior de idade");
+            }
+            }
+    
+            {  System.out.println("");
+            System.out.println("Exemplo 3 uso do 'if = else'");
+            int idade = 10;
+            if(idade < 16){
+                System.out.println("Proibido votar");
+                } else if (idade >= 18 && idade <= 70) {
+                    System.out.println("Voto obrigatorio");
+                } else if (idade == 16 || idade == 17 || idade > 70){
+                System.out.println("Voto facultativo");
+                }
+            }
+        }
+
+    
+        
+       
+       
+
