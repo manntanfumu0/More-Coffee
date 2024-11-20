ff3defb48c924501e0a6d6a921a69e492dc250a0
Manuel Ntanfumu <164122296+manntanfumu0@users.noreply.github.com>
2024-11-20 14:46:51 -0300

    Fundamentos da programação em Java parte 1
    Nomeando variáveis :
    
    - Não pode começar com números
    - Não pode usar palavras reservadas do Java: class, public, static, void.
    - Não usar caracteres especiais e nem deixar espaços
    - Definir nomes que descrevam o tipo de informação que as variáveis irão armazenar
    
    
    O arquivo possui um exemplo simples da nomeação de uma variável do tipo "String";

---
+++ Fundamentos.java
@@ -0,0 +1,10 @@
+package cursoJava;
+
+public class Fundamentos {
+
+    public static void main(String[] args) {
+        String nome;
+        nome = "Manuel Filipe";
+        System.out.println("Nome: " + nome);
+    }
+}
