65da9b0f07b0caf8b1a34d3000cadc85be5c7bf8
Manuel Ntanfumu <164122296+manntanfumu0@users.noreply.github.com>
2024-11-20 15:23:16 -0300

    Estruturas de Repetição (Loops)
    Estruturas de Repetição (Loops)
    Permitem executar um bloco de código várias vezes, com base em uma condição.
    a) while
    Executa o bloco enquanto a condição for verdadeira.

---
+++ Estruturarep1.java
@@ -0,0 +1,12 @@
+public class Estruturarep1 {
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("Exemplo 7 uso do whille");
+        int contador = 1;
+        while (contador <= 10) {
+            System.out.println(contador);
+            contador++;
+            
+        }
+    }
+}
